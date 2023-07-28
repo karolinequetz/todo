5b281215d812d569c03cc41a01c74e5dee604adb
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-07-28 15:44:35 -0300

    feat: created function to delete all items

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -9,7 +9,21 @@ import { TaskList } from '../../model/task-list';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-  public taskList: Array<TaskList> = [];
+  public taskList: Array<TaskList> = [
+    { task: 'Minha nova Task', checked: true },
+    { task: 'Minha nova Task 2', checked: false },
+  ];
   constructor() {}
   ngOnInit(): void {}
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm('Você deseja realmente Deletar tudo ?');
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
 }
